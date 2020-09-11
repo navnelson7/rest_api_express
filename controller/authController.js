eea925df7d7bcefaef55a8f5dcb4456dbdc6d039
Juan Navarro <navnelson7@gmail.com>
2020-09-10 21:12:18 -0600

    agregando endpoint para obtener usuario autenticado

--- controller/authController.js
+++ controller/authController.js
@@ -1,26 +1,26 @@
 const Usuario = require("../models/Usuario");
-const bcryptjs = require ('bcryptjs');
-const  { validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
-exports.autenticarUsuario = async (req, resp) =>{
+exports.autenticarUsuario = async(req, resp) => {
     //revisar si hay errores
     const errores = validationResult(req);
-    if(!errores.isEmpty()){
-        return resp.status(400).json({errores: errores.array()});
+    if (!errores.isEmpty()) {
+        return resp.status(400).json({ errores: errores.array() });
     }
     //extaer el email y password
     const { email, password } = req.body;
     try {
         //revisar que sea un usuario registrado
         let usuario = await Usuario.findOne({ email });
-        if(!usuario){
-            return resp.status(400).json({ msg: 'El usuario no existe'});
+        if (!usuario) {
+            return resp.status(400).json({ msg: 'El usuario no existe' });
         }
         //revisar el password
         const passCorrecto = await bcryptjs.compare(password, usuario.password);
-        if(!passCorrecto){
-            return resp.status(400).json({ msg: 'Password Incorrecto'});
+        if (!passCorrecto) {
+            return resp.status(400).json({ msg: 'Password Incorrecto' });
         }
 
         //si todo es correcto 
@@ -31,14 +31,24 @@ exports.autenticarUsuario = async (req, resp) =>{
             }
         };
 
-        jwt.sign(payload, process.env.SECRETA,{
-            expiresIn:3600
-        }, (error, token)=>{
-            if(error) throw error;
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600
+        }, (error, token) => {
+            if (error) throw error;
             //mensaje de confirmacion
-            resp.json({token});
+            resp.json({ token });
         });
     } catch (error) {
         console.log(error);
+    }
+}
+
+//obtiene que usuairo esta autenticado
+exports.usuarioAutenticado = async(req, resp) => {
+    try {
+        const usuario = await Usuario.findById(req.usuario.id);
+    } catch (error) {
+        console.log(error);
+        resp.status(500).json({ msg: 'Hubo un error' });
     }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,21 @@
 //rutas para autenticar usuarios
 const express = require('express');
 const router = express.Router();
-const {check} = require('express-validator');
+const { check } = require('express-validator');
 const authController = require('../controller/authController');
-//crear un usuario
+const auth = require('../middleware/auth');
+
+//iniciar sesion
 //api/auth
-router.post('/', 
-    [
+router.post('/', [
         check('email', 'Agregar un correo valido').isEmail(),
-        check('password','El password debe ser minimo de 6 caracteres').isLength({min:6})
+        check('password', 'El password debe ser minimo de 6 caracteres').isLength({ min: 6 })
     ],
     authController.autenticarUsuario
 );
 
+router.get('/',
+    auth,
+    authController.usuarioAutenticado
+);
 module.exports = router;
